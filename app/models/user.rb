72628e9a6d11663b9362db97d9b2268e830700ed
”二ノ宮大翼” <daisukeninomiya0375@gmail.com>
2019-12-13 09:27:01 +0000

    no18 task

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  
+  #名前の長さに制限を追加する
+  validates :name, presence: true, length: {maximum: 15}
+  #メールアドレスの正規表現を追加してください
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  #パスワードの文字数制限を追加する 8〜32文字
+  #パスワードをアルファベット、数字の混合のみ可能にしてください
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,32}+\z/ }
   has_secure_password
+  
   
 end
